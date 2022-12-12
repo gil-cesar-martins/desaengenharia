c1914ad88f02316eed679876836c12c0561006da
gil-cesar-martins <gilcesartst@gmail.com>
2022-12-12 08:13:29 -0300

    Retirada do subdomínio

--- desa/settings.py
+++ desa/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-%*h(qr@m9!4p_x$ivuyo_6^-as9dq_#*(ecm!y)yb0j5=u4+am
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['desaengenharia.com.br','www.desaengenharia.com.br', '127.0.0.1']
+ALLOWED_HOSTS = ['desaengenharia.com.br','127.0.0.1']
 
 # Application definition
 
