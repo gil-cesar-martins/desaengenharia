c06414fbcf7ff46eaed84986e0e4590b0dd2d97d
gil-cesar-martins <gilcesartst@gmail.com>
2023-08-21 08:49:04 -0300

    Alterações no wsgi

--- desaengenharia/wsgi.py
+++ desaengenharia/wsgi.py
@@ -2,6 +2,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling, MediaCling
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'desasite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'desaengenharia.settings')
 
 application = Cling(MediaCling(get_wsgi_application()))
