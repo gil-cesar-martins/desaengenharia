8db60875b2266d74d46e7fcd0939f179b3c4c8ba
gil-cesar-martins <gilcesartst@gmail.com>
2023-08-22 09:04:57 -0300

    Alteração no ALLOWED_HOSTS v2

--- desaengenharia/settings.py
+++ desaengenharia/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','20.206.176.5','.desaengenharia.com.br']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','20.206.176.5','desaengenharia.com.br']
 
 # Application definition
 
